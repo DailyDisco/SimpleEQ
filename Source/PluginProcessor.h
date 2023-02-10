d0538d28f944290a72777d5ce492909c32ed0736
Diego (D) <actual.mensch0r@icloud.com>
2023-02-09 23:48:36 -0600

    connect low cut band filter and controls

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,12 +10,21 @@
 
 #include <JuceHeader.h>
 
+enum Slope
+{
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
 //create a data structure to keep your code nice and readable
 struct ChainSettings
 {
     float peakFreq { 0 }, peakGainInDecibels{ 0 }, peakQuality {1.f};
     float lowCutFreq { 0 }, highCutFreq { 0 };
-    int lowCutSlope { 0 }, highCutSlope { 0 };
+    
+    int lowCutSlope { Slope::Slope_12 }, highCutSlope { Slope::Slope_12 };
 };
 
 //write a helper function to get all these values
